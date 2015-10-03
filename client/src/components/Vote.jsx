0f0df71927270e131fcc067377d25f7e5f6cde8b
calderas <calderas@gmail.com>
2015-10-03 15:14:24 -0400

    add PureRenderMixin

--- client/src/components/Vote.jsx
+++ client/src/components/Vote.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React from 'react/addons';
 
 export default React.createClass({
+  mixins: [React.addons.PureRenderMixin],
+
   getPair: function() {
     return this.props.pair || [];
   },
