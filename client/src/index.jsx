75915e1952c11d7c976eb4d60f5926f3f8042dac
calderas <calderas@gmail.com>
2015-10-03 13:43:29 -0400

    add ui components

---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+React.render(
+  <Voting pair={pair} hasVoted="Trainspotting" winner="Trainspotting" />,
+  document.getElementById('app')
+)
